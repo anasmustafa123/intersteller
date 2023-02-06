d3d08750d102654358798823ce136f55603a4d0a
anas mustafa <anasmuostafa@gmail.com>
2023-02-06 09:13:02 +0200

    adding click sound to the button

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
-/*
 
-const btn = document.querySelector("button");
-btn.addEventListener('click',  clikButton);
-
-
-
-
-
-*/
 function removeTransition(event){
     if(event.propertyName !== "transform") return;
     event.target.classList.remove('bushButton');
 }
-function clikButton(btn){
+function clikButton(){
     this.classList.add("bushButton");
-    console.log("asdf");
+}
+function clickSound(){
+    const audio = document.querySelector('audio');
+    audio.currentTime = 0;
+    if (!audio) return;
+    audio.play();
 }
 const btns = document.querySelectorAll("button");
 
 btns.forEach( (btn) => {
     btn.addEventListener('click',  clikButton);
     btn.addEventListener("transitionend" , removeTransition);
+    btn.addEventListener('click', clickSound);
+
 })  
 
 
